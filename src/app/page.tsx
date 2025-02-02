84413102fe7342f5ac1bb73baa416edbec7daa4e
javohirbro <javohirtech@gmail.com>
2025-02-02 13:55:07 +0500

    fix: mistakes and optimize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,9 @@ const Page = () => {
   return (
       <div>
         <div className={"my-container"}>
+          <h1 className={"text-2xl font-bold mb-5"}>Next-Leaflet with multiple tiles</h1>
           <Map/>
-          <div>
-            <h1 className={"text-2xl font-bold"}>
-              Next.js Leaflet Map with Multiple Tile Layers
-            </h1>
-            <p className={"text-lg mt-4"}>
-              This project demonstrates how to integrate Leaflet with Next.js to create an interactive map that supports
-              multiple tile layers. The example includes:
-
-              Setting up Leaflet in a Next.js environment
-              Adding a dynamic map component
-              Switching between multiple tile layers (e.g., OpenStreetMap, Google Maps, Satellite, etc.)
-              Customizing the map with markers, popups, and other interactive features
-              This implementation ensures optimal performance by handling map rendering efficiently in a Next.js
-              application.
-            </p>
-          </div>
+          <hr/>
         </div>
       </div>
   )
